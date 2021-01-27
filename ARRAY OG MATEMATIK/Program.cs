42120ecdabdb97d5e35afa29d3e1cfa7ad86cfa9
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-27 15:23:26 +0100

    Add project files.

---
+++ ARRAY OG MATEMATIK/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY_OG_MATEMATIK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listeB = new List<int>();
+
+            
+
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listeB.Add(i);
+                    
+                }
+            }
+
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    listeB.Remove(i);
+
+                }
+            }
+
+
+            List<int> listeC = new List<int>(listeB);
+
+            listeC.Reverse();
+
+
+
+            foreach (int number in listeB)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("den omvendte liste");
+
+            foreach (int number in listeC)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
